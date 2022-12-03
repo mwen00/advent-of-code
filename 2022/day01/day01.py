d1cd8de712538e7ea73f4d994b9b01785fbb9464
mwen00 <melanie.a.wen@gmail.com>
2022-12-02 18:02:20 -0800

    add tests and add day2

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -9,31 +9,50 @@ Get the total Calories carried by the top three Elves carrying the most Calories
 That way, even if one of those Elves runs out of snacks, they still have two backups.
 """
 
-TEST_FILE = "./2022/day01/test.txt"
-INPUT_FILE = "./2022/day01/input.txt"
 
-TEST_ANSWER = [24000, 11000, 6000]
+INPUT_FILE = "./day01/input.txt"
 
-top_three = []
 
-with open(INPUT_FILE, "r") as reader:
-    data = reader.read()
-    max = 0
-    sum_ = 0
+def calc_top_three(file: str) -> int:
+    top_three = []
 
-    for i in data.split("\n"):
-        if i == "":
-            max = sum_ if sum_ > max else max
-            top_three.append(sum_)
-                
-            sum_ = 0
-            continue
+    with open(file, "r") as reader:
+        data = reader.read()
+        max = 0
+        sum_ = 0
 
-        sum_ += int(i)
+        for i in data.split("\n"):
+            if i == "":
+                max = sum_ if sum_ > max else max
+                top_three.append(sum_)
+                    
+                sum_ = 0
+                continue
 
-    print(f"Max: {max}")
+            sum_ += int(i)
 
-top_three.sort(reverse=True)
-del top_three[3:]
+    top_three.sort(reverse=True)
+    del top_three[3:]
 
-print(f"Top Three Total: {sum(top_three)}")
+    return sum(top_three)
+
+
+def main():
+    print(calc_top_three(INPUT_FILE))
+
+
+# Small Test 
+import unittest
+
+class Day1Test(unittest.TestCase):
+    TEST_FILE = "./day01/test.txt"
+    TOP_THREE = [24000, 11000, 6000]
+
+    def testCalcTopThree(self):
+        result = calc_top_three(Day1Test.TEST_FILE)
+        self.assertEqual(sum(Day1Test.TOP_THREE), result)
+
+
+if __name__ == "__main__":
+    main()
+    unittest.main()
